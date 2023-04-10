e5b258afdc8bfd7d63147c4f6fefd0edd38d8f2a
froststar21 <DEATHOFALLPROPORTIONS@YAHOO.COM>
2023-04-09 20:04:34 -0500

    Display backend data in alert

--- Learning-React/src/App.tsx
+++ Learning-React/src/App.tsx
@@ -3,6 +3,8 @@ import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import ButtonThing from "./components/ButtonThing";
 import DataGrabButton from "./components/DataGrabButton";
+import axios from 'axios';
+
 
 function App()
 {
@@ -24,9 +26,21 @@ function App()
     console.log('Clicked Button of Type: ' + type + " \nMessage on Button is: " + children);
   }
 
+  const fetchBackendData = async () => 
+  {
+      try {
+        const response = await axios.get('http://localhost:5030/api/BasicTest/message');
+        console.log('Data from the backend:', response.data);
+        setAlertText(response.data)
+      } catch (error) {
+        setAlertText("Error Fetching Data")
+        console.error('Error fetching message:', error);
+      }
+  };
+
 
   const [alertVisable, setAlertVisability] = useState(false)
-
+  const [alertText, setAlertText] = useState("Default Text")
   return (
     <div>
       <ListGroup 
@@ -34,14 +48,14 @@ function App()
         heading="NumberStrings"
         onSelectItem={handleSelectItem} 
       />
-      {alertVisable && <Alert onClose={() => {setAlertVisability(false)}}>Suh but different</Alert>}
+      {alertVisable && <Alert onClose={() => {setAlertVisability(false)}}>{alertText}</Alert>}
       <ButtonThing onClick={() => {
         printButtonInfo; 
         setAlertVisability(true);
         }}>
         MyButton
       </ButtonThing>
-      <DataGrabButton />
+      <DataGrabButton fetchMessage={() => {fetchBackendData()}}/>
     </div>
   );
 }
--- Learning-React/src/components/DataGrabButton.tsx
+++ Learning-React/src/components/DataGrabButton.tsx
@@ -1,18 +1,10 @@
-import axios from 'axios';
 import React from 'react'
 
-const DataGrabButton = () => {
-
-    const fetchMessage = async () => 
-    {
-        try {
-          const response = await axios.get('http://localhost:5030/api/BasicTest/message');
-          console.log('Data from the backend:', response.data);
-        } catch (error) {
-          console.error('Error fetching message:', error);
-        }
-    };
-
+interface Props
+{
+  fetchMessage: () => void;
+}
+const DataGrabButton = ({fetchMessage}: Props) => {
 
     return (
         <button className="btn btn-primary" onClick={fetchMessage}>
